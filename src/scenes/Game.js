ce912561049f4449e9c64864cbfcfa175efb2bda
Maksim Khortov <xoptov@mail.ru>
2021-06-02 17:54:58 +0300

    Makes some changes in game scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,16 +2,56 @@ import Phaser from '../lib/phaser.js'
 
 export default class Game extends Phaser.Scene
 {
-    constructor()
-    {
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    player;
+
+    constructor() {
         super('game');
     }
-    preload()
-    {
+
+    preload() {
         this.load.image('background', 'assets/bg_layer1.png');
+        this.load.image('platform', 'assets/ground_grass.png');
+        this.load.image('bunny-stand', 'assets/bunny1_stand.png');
     }
-    create()
-    {
+
+    create() {
         this.add.image(240, 320, 'background');
+        
+        const platforms = this.physics.add.staticGroup();
+
+        for (let i = 0; i < 5; i++) {
+            const x = Phaser.Math.Between(80, 400);
+            const y = 150 * i;
+
+            /** @type {Phaser.Physics.Arcade.Sprite} */
+            const platform = platforms.create(x, y, 'platform');
+            platform.setScale(0.5);
+
+            /** @type {Phaser.Physics.Arcade.StaticBody} */
+            const body = platform.body;
+            body.updateFromGameObject();
+        }
+
+        this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+        this.physics.add.collider(platforms, this.player);
+
+        this.player.body.checkCollision = {
+            none: false,
+            up: false,
+            down: true,
+            left: false,
+            right: false
+        };
+
+        this.cameras.main.startFollow(this.player);
+    }
+
+    update() {
+        const touchingDown = this.player.body.touching.down;
+
+        if (touchingDown) {
+            this.player.setVelocityY(-300);
+        }
     }
 }
