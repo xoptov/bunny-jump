501ef87f26d6d1fe23b1b9d1ea1ffc5d51f36275
Maksim Khortov <xoptov@mail.ru>
2021-06-03 16:00:00 +0300

    Added some logic to game

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,8 +2,14 @@ import Phaser from '../lib/phaser.js'
 
 export default class Game extends Phaser.Scene
 {
+    /** @type {Phaser.Physics.Arcade.StaticGroup} */
+    platforms;
+
     /** @type {Phaser.Physics.Arcade.Sprite} */
     player;
+
+    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+    cursors;
 
     constructor() {
         super('game');
@@ -13,19 +19,20 @@ export default class Game extends Phaser.Scene
         this.load.image('background', 'assets/bg_layer1.png');
         this.load.image('platform', 'assets/ground_grass.png');
         this.load.image('bunny-stand', 'assets/bunny1_stand.png');
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create() {
-        this.add.image(240, 320, 'background');
+        this.add.image(240, 320, 'background').setScrollFactor(1, 0);
         
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
 
         for (let i = 0; i < 5; i++) {
             const x = Phaser.Math.Between(80, 400);
             const y = 150 * i;
 
             /** @type {Phaser.Physics.Arcade.Sprite} */
-            const platform = platforms.create(x, y, 'platform');
+            const platform = this.platforms.create(x, y, 'platform');
             platform.setScale(0.5);
 
             /** @type {Phaser.Physics.Arcade.StaticBody} */
@@ -34,7 +41,7 @@ export default class Game extends Phaser.Scene
         }
 
         this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
-        this.physics.add.collider(platforms, this.player);
+        this.physics.add.collider(this.platforms, this.player);
 
         this.player.body.checkCollision = {
             none: false,
@@ -44,14 +51,44 @@ export default class Game extends Phaser.Scene
             right: false
         };
 
-        this.cameras.main.startFollow(this.player);
+        this.cameras.main.startFollow(this.player, true);
+        this.cameras.main.setDeadzone(this.scale.width * 1.5);
     }
 
-    update() {
+    update(t, dt) {
+        this.platforms.children.iterate(child => {
+            const platform = child;
+            const scrolly = this.cameras.main.scrollY;
+            if (platform.y >= scrolly + 700) {
+                platform.y = scrolly - Phaser.Math.Between(50, 100);
+                platform.body.updateFromGameObject();
+            }
+        });
+
         const touchingDown = this.player.body.touching.down;
 
         if (touchingDown) {
             this.player.setVelocityY(-300);
+        }
+
+        if (this.cursors.left.isDown && !touchingDown) {
+            this.player.setVelocityX(-200);
+        } else if (this.cursors.right.isDown && !touchingDown) {
+            this.player.setVelocityX(200);
+        } else {
+            this.player.setVelocityX(0);
+        }
+
+        this.horizontalWrap(this.player);
+    }
+
+    horizontalWrap(sprite) {
+        const halfWidth = sprite.displayWidth * 0.5;
+        const gameWidth = this.scale.width;
+        if (sprite.x < -halfWidth) {
+            sprite.x = gameWidth + halfWidth;
+        } else if (sprite.x > gameWidth + halfWidth) {
+            sprite.x = -halfWidth;
         }
     }
 }
